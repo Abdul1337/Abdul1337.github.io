570045034f27692cd8733e74e36d217c49f8bf45
Abdul1337 <abdulaziz1337.99@gmail.com>
2020-06-08 15:55:53 +0530

    Form integration Complete

--- projects/techtalisman/static/js/main.js
+++ projects/techtalisman/static/js/main.js
@@ -23,3 +23,52 @@ $(".slick-institute").slick(
     }
 );
 
+
+// Form Submission
+var url = "https://script.google.com/macros/s/AKfycbxWQfbWKf9QyROa6SBHsxXvn5_YFSiu-h9n7ssPhB6IpWC6stqv/exec";
+$.ajaxSetup({
+    async: !0
+})
+
+const handleReffFormSubmit = t => {
+    t.preventDefault();
+    const e = formToJSON(reff.elements);
+    console.log("Data : ",e)
+
+    $.ajax({
+        url: url,
+        method: "GET",
+        dataType: "json",
+        data: e,
+        error: function () {
+            setTimeout(() => {
+                $('#form_status').css({"color" : "#f00"})
+                $('#form_status').html("Somthing Went Wrong! Please Try again later");
+                $("#form_submit_btn").html("Book a Free Demo");
+                resetForm(t.target);
+            }, 500)
+        },
+        success: function () {
+            setTimeout(() => {
+                $('#form_status').css({"color" : "#16d316"})
+                $('#form_status').html("Thank You! Your Response has been Submitted");
+                $("#form_submit_btn").html("Submitted");
+                resetForm(t.target);
+
+            }, 500)
+        }
+    });
+
+    $("#form_submit_btn").html("Submitting Please Wait");
+
+    return false;
+}
+
+formToJSON = t => [].reduce.call(t, (t, e) => (t[e.name] = e.value, t), {}),
+reff = document.getElementById("BookForm");
+console.log("reff");
+function resetForm(t) {
+    console.log("t",t);
+    for (i in t){t[i].value = ""}
+}
+reff.addEventListener("submit", handleReffFormSubmit);
