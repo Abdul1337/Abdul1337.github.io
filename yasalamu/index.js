8d08ca045948aa053c460f860e74a43359023756
Abdul1337 <abdulaziz1337.99@gmail.com>
2021-07-06 19:51:29 +0530

    Ya Salamu Project V 2.0 New Look

--- yasalamu/index.js
+++ yasalamu/index.js
@@ -1,13 +1,26 @@
 console.log("Helllllo")
 
-count = 0
+var count = 0
+var max = 9999
+var increment = 1
 
-document.getElementById("btn").addEventListener("click", function() {
-    count += 1
-    document.getElementById("count").innerHTML = count;
-});
+document.getElementById("btn").addEventListener("click", ()=>increamentCount());
+
+const increamentCount = () => {
+    if(count < max){
+        count += increment
+        document.getElementById("count").innerHTML = count;
+    }else{
+        document.getElementById("count").innerHTML = 'MAX';
+    }
+}
 
 document.getElementById("reset").addEventListener("click", function() {
     count = 0
     document.getElementById("count").innerHTML = count;
+});
+
+
+document.getElementById("control_input").addEventListener("change", function(e) {
+    increment = parseInt(e.target.value);
 });
