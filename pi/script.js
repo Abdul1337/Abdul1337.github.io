004e89b064a7cf7fc1ca3538280f9b8471320cb5
Abdul1337 <abdulaziz1337.99@gmail.com>
2021-10-25 18:02:38 +0530

    PI Project UI update

--- pi/script.js
+++ pi/script.js
@@ -2,22 +2,72 @@ const PI = "3.141592653589793238462643383279502884197169399375105820974944592307
 
 $keys = document.querySelectorAll('.num');
 $input = document.getElementById('input');
+$clearBtn = document.getElementById('clearBtn');
+$finishBtn = document.getElementById('finishBtn');
+$resultsCloseBtn = document.getElementById('resultsCloseBtn');
+
+$score = document.getElementById('score');
+$mistakes = document.getElementById('mistakes');
+$resultsBox = document.getElementById('resultsBox');
+
+
+$clearBtn.addEventListener('click', clearAll)
+$resultsCloseBtn.addEventListener('click', closeResultBox)
+$finishBtn.addEventListener('click', finishAndShowResults)
 
 $keys.forEach(key => {
     key.addEventListener('click', handleNumPress)
 });
 
 let current_digit = 2
+let current_score = 0
+let current_mistakes = 0
 
 function handleNumPress(e){
-    console.log(e)
+    console.log({
+        current_score, current_mistakes
+    })
     digit = e.target.dataset['number']
-    console.log(digit)
-    console.log("Curr PI : ", PI[current_digit])
     if (PI[current_digit] === digit){
         $input.innerHTML += digit
         current_digit += 1
+        current_score += 1
     }else{
-        alert('Wrong!')
+        current_mistakes += 1
     }
+
+    updateScore({
+        current_score,
+        current_mistakes
+    })
+}
+
+function updateScore(score){
+    $score.innerHTML = score.current_score;
+    $mistakes.innerHTML = score.current_mistakes;
+}
+
+function clearAll(){
+    if (confirm('Are you sure you want to clear?')) {
+        $input.innerHTML = "3."
+        current_score = 0;
+        current_mistakes = 0;
+        updateScore({current_score, current_mistakes})
+    }
+}
+
+function finishAndShowResults(){
+    if (current_score > 0){
+        let accuracy = (100 - (current_mistakes/current_score)*100).toFixed(2)
+        $resultsBox.querySelector('#stats_score>span.value').innerHTML = current_score + ' digits'
+        $resultsBox.querySelector('#stats_mistakes>span.value').innerHTML = current_mistakes
+        $resultsBox.querySelector('#stats_accuracy>span.value').innerHTML = accuracy >= 0 ? accuracy + '%' : '0%' 
+        $resultsBox.style.transform = 'translate(-50%, -50%)'
+    }else{
+        alert('Score should be atleat 1')
+    }
+}
+
+function closeResultBox(){
+    $resultsBox.style.transform = 'translate(-50%, -500%)'
 }
